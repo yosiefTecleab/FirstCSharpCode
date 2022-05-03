0dff6007e999f8c944a04495eaa789cbc25af06b
yosie <yosie@LAPTOP-N194SGOT>
2022-05-03 18:28:12 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstCSharpCode
+{
+    class Book
+    {
+        private string title;
+        public string author;
+        public int page;
+        public Book(string title, string author, int page)
+        {
+            this.Title = title;
+            this.author = author;
+            this.page = page;
+        }
+
+        public string Title
+        { 
+            get{ return title; } 
+            set
+            {
+                title = value;
+            } 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FirstCSharpCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+            Book b1 = new Book("Harry Potter", "Rowlen",400);
+
+
+            Console.WriteLine(b1.Title);
+
+            //Console.ReadLine();
+
+            Book[] shelve = {new Book("Moon Light","Unknown",200),
+            new Book("Sun","Known",150), new Book("The earth","Glob",567)
+
+            };
+
+            foreach (Book book in shelve)
+            {
+                Console.WriteLine(book.Title+" "+book.page);
+            }
+            
+        }
+    }
+}
